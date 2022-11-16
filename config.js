6c076c76f5b2a4746f9ca5e071677afb3129a4be
musganz01 <96802092+musganz@users.noreply.github.com>
2022-11-16 17:50:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,30 +3,30 @@
 // JANGAN DIHAPUS KALAU MAU DITAMBAHIN
 
 // NUMBER
-global.owner = ['62895327934887'] 
+global.owner = ['6281803826849'] 
 global.mods = [] 
 global.prems = [] 
-global.kontak = ['62895327934887']
+global.kontak = ['6281803826849']
 
 //  INFO BOT 
-global.naown = 'KRIZYN OFC'
-global.noown = '62895327934887'
-global.namebot = 'FORYN FOUR'
-global.nobot = '6289502319715'
+global.naown = 'EXSAUDIA'
+global.noown = '6281803826849'
+global.namebot = 'TEAM JU'
+global.nobot = '6281803826849'
 
 //  URL SOSMED
 global.instagram = 'https://instagram.com/itskri99'
 global.github = 'https://github.com/krizynofc'
-global.gc = 'https://chat.whatsapp.com/HVyRaNWp18NKMAIHv8NjLP'
+global.gc = 'https://chat.whatsapp.com/J316oJzk9tK6LJJr2veNqU'
 global.web = 'https://lynk.id/kri.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.saweria = 'https://saweria.co/Kricom'
 
 // PAYMANT 
-global.dana = '081360482998'
-global.pulsa = '081360482998'
-global.gopay = '081360482998'
-global.shopay = '081360482998'
-global.ovo = '081360482998'
+global.dana  = '085721354560'
+global.pulsa = '085721354560'
+global.gopay = '085721354560'
+global.shopay = '085721354560'
+global.ovo = '085721354560'
 
 // PROFILE ALL 
 global.ppreg = 'https://telegra.ph/file/445721f7cdab701543840.jpg'
